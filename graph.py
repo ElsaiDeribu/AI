b5ed98095142b64fa5a99e2565e6b28f9b1424b4
Elsai Deribu <elsaideribu7@gmail.com>
2022-05-26 15:01:42 +0300

    made an algorithm to find the sortest path using dfs and bfs

--- graph.py
+++ graph.py
@@ -21,17 +21,18 @@ class Graph:
         self.verticies = {}
         self.edges = {}
 
+
     def add_node(self, node):
         self.verticies[node.name] = node
     
-    def add_edge(self, left, right, weight=1):
+    def add_edge(self, left, right, weight = 1):
         if left.name not in self.verticies:
             self.verticies[left.name] = left
         
         if right.name not in self.verticies:
             self.verticies[right.name] = right
 
-        e = Edge(left, right, weight=1)
+        e = Edge(left, right, weight)
 
 
         key = (left.name, right.name)
@@ -40,45 +41,86 @@ class Graph:
         key = (right.name, left.name)
         self.edges[key] = e
 
-        left.connect(right)
-        right.connect(left)
+        self.verticies[left.name].connect(right)
+        self.verticies[right.name].connect(left)
         
-    def dfs (self, startNode, requiredNode):
-        if startNode.name == requiredNode.name:
-            return startNode
-        visited = set()
-        st = []
-        st.append(startNode.name)
-        while st:
-            v = st.pop()
-            if v not in visited:
-                visited.add(v)
-            for _, neighbourName in self.verticies[v].edge_list:
-                if neighbourName == requiredNode.name:
-                    return self.verticies[neighbourName]
-                if neighbourName not in visited:
-                    st.append(neighbourName)
-        return 0
+        # print(self.verticies[left.name].edge_list)
+        # print(left.edge_list)
+        
+        
+        
+    def dfs(self, startNode, requiredNode):
+        
+        self.shortest = [1] * power(10,5)
+        path = []
+        
+        def helper(givenNode, requiredNode, path):
+            path.append(givenNode.name)
+            if givenNode.name == requiredNode.name:
+                if len(path) < len(self.shortest):
+                    self.shortest = path[:]
+            
+            for neighbor in givenNode.edge_list:
+                if neighbor[1] not in path:
+                    helper(self.verticies[neighbor[1]], requiredNode, path[:])
+        
+            path.pop()
+                    
+        helper(startNode, requiredNode, path)
+        
+        return self.shortest
+    
+        
+    # def dfs (self, startNode, requiredNode):
+        
+    #     if startNode.name == requiredNode.name:
+    #         return startNode
+    #     path = [] 
+    #     visited = set()
+    #     st = []
+    #     st.append(startNode.name)
+    #     path.append(startNode.name)
+    #     while st:
+    #         v = st.pop()
+    #         if v not in visited:
+    #             visited.add(v)
+    #             path.append(v)
+    #         for _, neighbourName in self.verticies[v].edge_list:
+    #             if neighbourName == requiredNode.name:
+    #                 return self.verticies[neighbourName]
+    #             if neighbourName not in visited:
+    #                 st.append(neighbourName)
+    #     return 0
+    
+    
+    
     
     def bfs(self, startNode, requiredNode):
-        if startNode.name == requiredNode.name:
-            return startNode
         visited = set()
         queue = deque()
-        queue.append(startNode.name)
+        queue.append((startNode.name, []))
         visited.add(startNode.name)
+        
         while queue:
-            v = queue.popleft()
-            for _,neighbour in self.verticies[v].edge_list:
-                if neighbour == requiredNode.name:
-                    return self.verticies[neighbour]
+            v, path = queue.popleft()
+            path.append(v)
+            if v == requiredNode.name:
+                return path
+            for _, neighbour in self.verticies[v].edge_list:
                 if neighbour not in visited:
-                    queue.append(neighbour)
+                    queue.append((neighbour, path[:]))
                     visited.add(neighbour)
+    
+            
+        print(visited)
             
         return 0
+    
+    def djikstra(startNode, destination):
+        shortest_distance = {}
         
-
+        
+        
     def search(self, a, b):
         pass
 
@@ -86,11 +128,19 @@ class Graph:
         pass
 
 
+
+
+
+
+
+
 a = Node('A')
 b = Node('B')
-c = Node('C')
+c = Node('C') 
 d = Node('D')
 e = Node('E')
+k = Node('K')
+l = Node('L')
 
 g = Graph()
 g.add_edge(a, e)
@@ -98,12 +148,76 @@ g.add_edge(a, c)
 g.add_edge(a, d)
 g.add_edge(b, c)
 g.add_edge(b, e)
+g.add_edge(b, l)
+g.add_edge(l, k)
+g.add_edge(k, e)
 
 # print(g)
-for iv, (k, v) in enumerate(g.verticies.items()):
-    print(v.name)
+# for iv, (k, v) in enumerate(g.verticies.items()):
+#     print(v.name)
 
-for iv, (k, edge) in enumerate(g.edges.items()):
-    print(edge.right.name, edge.left.name, edge.weight)
+# for iv, (k, edge) in enumerate(g.edges.items()):
+#     print(edge.right.name, edge.left.name, edge.weight)
 
 
+
+
+# ans = g.bfs(d,l)
+
+# print(ans)
+
+
+
+m = Graph()
+
+file = open('graph.txt', 'r')
+f = file.readlines()
+
+readEdges = []
+for line in f:
+    readEdges.append(line.strip())
+        
+# print(readEdges)
+
+for connection in readEdges:
+    temp = connection.split(' ')
+    
+    if len(temp) == 3:
+        edge = temp
+        a = Node(edge[0])
+        b = Node(edge[1])
+        m.add_edge(a, b, int(edge[2]))
+        
+    else:
+        node = temp
+        a = Node(node[0])
+        m.add_node(a)
+    
+
+
+
+a = m.verticies["Neamt"]
+b = m.verticies["Craiova"]
+
+
+ans = m.bfs(a, b)
+print(ans)
+
+# ans = m.dfs(a, b)
+# print(ans)
+
+
+
+
+
+
+
+
+
+
+
+# for iv, (k, v) in enumerate(m.verticies.items()):
+#     print(v.name)
+
+# for iv, (k, edge) in enumerate(m.edges.items()):
+#     print(edge.right.name, edge.left.name, edge.weight)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ans = m.bfs(a, b)
+print(ans)
